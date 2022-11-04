4df96df3e6775b38cd897f5ff0152cf78de647ae
Joseph Martineau <95492284+j-martineau@users.noreply.github.com>
2022-11-04 09:08:16 -0400

    cleaning up documentation

--- R/maxnch.R
+++ R/maxnch.R
@@ -1,5 +1,15 @@
+#' @name maxnch_uj
 #' @title Length of longest string
-#' @param x Character vector.
+#' @family strings
+#' @param x Atomic character object.
 #' @return Integer scalar.
 #' @export
-maxnch <- function(x) {max(nch(x))}
+maxnch_uj <- function() {help("maxnch_uj", package = "uj")}
+
+#' @describeIn maxnch_uj Gets the number of characters in the longest string in
+#'   \code{x}.
+#' @export
+maxnch <- function(x) {
+  if (!ichr(x)) {stop(" • [x] must be an atomic character object.")}
+  max(nch(x))
+}
